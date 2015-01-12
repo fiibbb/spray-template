26564a95e89e95bae0b97b9123db593b25b91897
Ling Yuan <lyuan@comprehend.com>
2015-01-12 14:57:53 -0800

    tmp

--- src/main/scala/com/example/DBTest.scala
+++ src/main/scala/com/example/DBTest.scala
@@ -13,4 +13,21 @@ trait DBTest {
   val db = Database.forURL("jdbc:postgresql:test:example", driver = "org.postgresql.Driver")
   implicit val session = db.createSession
 
+  class TestModel(tag:Tag) extends Table[(Long, String)](tag,"test_model") {
+    def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+    def value = column[String]("value",O.NotNull)
+    def * = (id,value)
+  }
+
+  val models = TableQuery[TestModel]
+  models.ddl.create
+
+  private val autoInc = models.map(m => m.value) returning models.map(_.id) into {
+    case (_,id) => id
+  }
+
+  def insert(s:String)(implicit session:Session) = {
+    val id = autoInc.insert(s)
+  }
+
 }
